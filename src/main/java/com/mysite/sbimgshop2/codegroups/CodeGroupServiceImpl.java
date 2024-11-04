0a814d6b81988e4847581aef457b036c7400fed6
mila811230 <bluegalaxy1230@gmail.com>
2024-11-04 16:05:36 +0900

    DUPLICATE_KEY

--- src/main/java/com/mysite/sbimgshop2/codegroups/CodeGroupServiceImpl.java
+++ src/main/java/com/mysite/sbimgshop2/codegroups/CodeGroupServiceImpl.java
@@ -1,6 +1,11 @@
 package com.mysite.sbimgshop2.codegroups;
 
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
+
+import com.mysite.sbimgshop2.common.exception.BusinessException;
+import com.mysite.sbimgshop2.common.exception.ErrorCode;
+
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -12,6 +17,11 @@ public class CodeGroupServiceImpl implements CodeGroupService {
 	@Override
 	public void register(CodeGroupDTO codeGroupDTO) {
 		// TODO DuplicateKeyException
-		codeGroupMapper.create(codeGroupDTO);
+		try {
+			codeGroupMapper.create(codeGroupDTO);
+			
+		} catch (DuplicateKeyException e){
+			throw new BusinessException(ErrorCode.DUPLICATE_KEY, e);
+		}
 	}
 }
