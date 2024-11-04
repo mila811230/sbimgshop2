5b3e06bffec986d0ce1838569fa8cabca207ed73
mila811230 <bluegalaxy1230@gmail.com>
2024-11-04 12:29:58 +0900

    프로젝트 구성

---
+++ src/main/java/com/mysite/sbimgshop2/Sbimgshop2Application.java
@@ -0,0 +1,13 @@
+package com.mysite.sbimgshop2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbimgshop2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbimgshop2Application.class, args);
+	}
+
+}
